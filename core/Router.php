22040f3d940294b6d012be3fd152e27914f53a49
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-08-21 23:35:53 +0600

    create Request.php file to get the path and method, and in Router handle the callback function and call it if exist

--- core/Router.php
+++ core/Router.php
@@ -5,7 +5,13 @@ namespace app\core;
 class Router
 {
 
+    public Request $request;
     protected array $routes = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
     public function get (string $path, callable $callback)
     {
         $this->routes['GET'][$path] = $callback;
@@ -13,7 +19,18 @@ class Router
 
     public function resolve()
     {
-        $requestedMethod = $_SERVER['REQUEST_METHOD'];
-        dd($_SERVER);
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        // dd($this->routes);
+        $callback = $this->routes[$method][$path] ?? false;
+
+        if (!$callback) {
+            http_response_code(404);
+            echo 'Not Found';
+            exit;
+        }
+
+        echo call_user_func($callback);
+        // dd($callback);
     }
 }
